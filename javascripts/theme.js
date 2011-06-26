a7da816a30ffd3ff287e567463cd7833f6e5494d
Jakob Skjerning <jakob@mentalized.net>
2011-06-26 21:03:33 +0200

    Refactor/clean up javascript

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -5,108 +5,101 @@ function injectViewportMetaTag() {
   $$('head')[0].insert(meta);
 };
 
-document.observe("dom:loaded", function() {
-  //try {
-    function buildProjectMenuItem(project) {
-      var link = $(document.createElement('a'));
-      link.href = project.url;
-      link.innerHTML = project.name;
-      if (project.selected) {
-        link.addClassName('selected');
-      };
-      var li = $(document.createElement('li'));
-      li.appendChild(link);
-
-      return li;
+ProjectMenuBuilder = {
+  buildMenuItem: function(project) {
+    var link = $(document.createElement('a'));
+    link.href = project.url;
+    link.innerHTML = project.name;
+    if (project.selected) {
+      link.addClassName('selected');
     };
+    var li = $(document.createElement('li'));
+    li.appendChild(link);
 
-    function buildProjectList(projectSelector) {
-      var projects = getProjectsFromSelectElement(projectSelector);
-      var projectList = $(document.createElement('ul'));
-      projectList.addClassName('projects');
-      projectList.setStyle({ display: 'none' });
+    return li;
+  },
 
-      projects.each(function(project, index) {
-        projectList.appendChild(buildProjectMenuItem(project));
-      });
+  buildList: function(projectSelector) {
+    var projects = ProjectMenuBuilder.getProjects(projectSelector);
+    var projectList = $(document.createElement('ul'));
+    projectList.addClassName('projects');
+    projectList.setStyle({ display: 'none' });
 
-      return projectList;
-    };
+    projects.each(function(project, index) {
+      projectList.appendChild(ProjectMenuBuilder.buildMenuItem(project));
+    });
 
-    function buildProjectListMenuItem(projectSelector) {
-      var menuItem = $(document.createElement('li'));
+    return projectList;
+  },
 
-      var title = getTitleFromSelectElement(projectSelector);
-      menuItem.appendChild(buildProjectListToggle(title));
+  buildListMenuItem: function(projectSelector) {
+    var menuItem = $(document.createElement('li'));
 
-      var projectList = buildProjectList(projectSelector);
-      menuItem.appendChild(projectList);
+    var title = ProjectMenuBuilder.getTitle(projectSelector);
+    menuItem.appendChild(ProjectMenuBuilder.buildToggle(title));
 
-      return menuItem;
-    };
+    var projectList = ProjectMenuBuilder.buildList(projectSelector);
+    menuItem.appendChild(projectList);
 
-    function buildProjectListToggle(title) {
-      var toggle = $(document.createElement('a'));
-      toggle.href = '#'; // Makes it behave like a real link
-      toggle.innerHTML = title.replace('...', '&hellip;');
+    return menuItem;
+  },
 
-      $(toggle).observe('click', function(event) {
-        $(this).up('li').down('.projects').toggle();
-        $(this).toggleClassName('active');
+  buildToggle: function(title) {
+    var toggle = $(document.createElement('a'));
+    toggle.href = '#'; // Makes it behave like a real link
+    toggle.innerHTML = title.replace('...', '&hellip;');
 
-        event.stop();
-      });
+    $(toggle).observe('click', function(event) {
+      $(this).up('li').down('.projects').toggle();
+      $(this).toggleClassName('active');
+      event.stop();
+    });
 
-      return toggle;
-    };
-
-    function getProjectsFromSelectElement(element) {
-      var projectOptions = element.getElementsBySelector('option[value!=""]');
-      return projectOptions.collect(function(node) {
-        return {
-          url: node.value,
-          name: node.innerHTML,
-          selected: node.readAttribute('selected') == 'selected'
-        };
-      });
-    };
-
-    function getTitleFromSelectElement(element) {
-      var title = element.childElements().first().innerHTML;
-      return title;
-    };
-
-    function moveProjectSelectorToTopMenu(projectSelector, topMenuList) {
-      if (!projectSelector || !topMenuList) {
-        return false;
-      }
-
-      var menuItem = buildProjectListMenuItem(projectSelector);
-
-      // Insert the menu item as the first in top menu
-      $(topMenuList).insert({ top: menuItem });
-
-      // Make the projectList at least as wide as the menu item
-      var width = menuItem.getWidth();
-      $(menuItem).down('.projects').setStyle({ minWidth: width + 'px' });
-
-      // Remove the original select list
-      projectSelector.hide();
-    };
-
-    moveProjectSelectorToTopMenu(
-      $$('#quick-search select').first(),
-      $$('#wrapper #top-menu > ul').first()
-    );
-
-    injectViewportMetaTag();
-
-  // } catch(error) {
-  //   if (typeof(console) != 'undefined') {
-  //     console.error(error);
-  //   };
-  //   throw error;
-  // }
+    return toggle;
+  },
   
+  getProjects: function(element) {
+    var projectOptions = element.getElementsBySelector('option[value!=""]');
+    return projectOptions.collect(function(node) {
+      return {
+        url: node.value,
+        name: node.innerHTML,
+        selected: node.readAttribute('selected') == 'selected'
+      };
+    });
+  },
+  
+  getTitle: function(element) {
+    var title = element.childElements().first().innerHTML;
+    return title;
+  },
+  
+  // Creates a ul with links to all the users projects and add it to the top menu
+  moveProjectSelectorToTopMenu: function(projectSelector, topMenuList) {
+    if (!projectSelector || !topMenuList) {
+      return false;
+    }
+
+    var menuItem = ProjectMenuBuilder.buildListMenuItem(projectSelector);
+
+    // Insert the menu item as the first in top menu
+    $(topMenuList).insert({ top: menuItem });
+
+    // Make the projectList at least as wide as the menu item
+    var width = menuItem.getWidth();
+    $(menuItem).down('.projects').setStyle({ minWidth: width + 'px' });
+
+    // Remove the original select list
+    projectSelector.hide();
+  }
+};
+
+document.observe("dom:loaded", function() {
+  ProjectMenuBuilder.moveProjectSelectorToTopMenu(
+    $$('#quick-search select').first(),
+    $$('#wrapper #top-menu > ul').first()
+  );
+
+  injectViewportMetaTag();
 });
 
